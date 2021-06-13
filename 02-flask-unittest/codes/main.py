17bb67026cc5b9125b9d1249721f03b6f0a2825b
Paul Chen <ggfu0114@gmail.com>
2021-06-13 21:31:21 +0800

    [FEAT] update the examples

--- 02-flask-unittest/codes/main.py
+++ 02-flask-unittest/codes/main.py
@@ -1,17 +1,17 @@
 from flask import Flask
-import sys
-from func import MathFunc
+
+from codes.func import MathFunc
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     mf = MathFunc()
-    px,py = mf.get_random_point()
-    res = mf.get_triangle_size((px,py))
+    px, py = mf.get_random_point()
+    res = mf.get_triangle_size((px, py))
     return f'Hello, gFu.\
             The size from p(0,0) to p({px},{py}) is {res}.'
-
 
 
 if __name__ == '__main__':
---
+++ 03-google-oauth/README.md
@@ -0,0 +1,3 @@
+## Preparation
+1. Download the firebase cred secret file and save it as `firebase-credential-file.json` file name. 
+2. Put the file into secret folder.
